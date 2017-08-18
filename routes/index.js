dfa1a0c012d2f73c7a33ab977bbb1b215b307141
Yu Zhou <zy_0710@outlook.com>
2017-08-18 14:42:48 +0800

    add device policies

--- routes/index.js
+++ routes/index.js
@@ -40,38 +40,54 @@ router.get('/policies', function (req, res, next) {
 });
 
 
-var c = 0;
+var stats = new Array(0, 0, 0, 0, 0);
 
 router.get('/s1', function (req, res, next) {
-    var str = "" + c + " Kbps";
-    c = c + 1;
+    update_stats();
+    var str = "" + stats[0] + " Kbps";
     res.send(str)
 });
 
 router.get('/s2', function (req, res, next) {
-    var str = "" + c + " Kbps";
-    c = c + 1;
+    var str = "" + stats[1] + " Kbps";
     res.send(str)
 });
 
 router.get('/s3', function (req, res, next) {
-    var str = "" + c + " Kbps";
-    c = c + 1;
+    var str = "" + stats[2] + " Kbps";
     res.send(str)
 });
 
 router.get('/s4', function (req, res, next) {
-    var str = "" + c + " Kbps";
-    c = c + 1;
+    var str = "" + stats[3] + " Kbps";
     res.send(str)
 });
 
 router.get('/s5', function (req, res, next) {
-    var str = "" + c + " Kbps";
-    c = c + 1;
+    var str = "" + stats[4] + " Kbps";
     res.send(str)
 });
 
+
+function update_stats() {
+    const options = {
+        hostname: '101.6.30.156',
+        port: 10888,
+        path: '/state',
+        method: 'get'
+    };
+    var reqest = http.request(options,function (data) {
+        data.on('data',function (chunk) {
+            var str = chunk.toString();
+            str = str.split('+')
+            for(var i =0 ; i< 5; i++) {
+                stats[i] = Number(str[i]) - stats[i];
+            }
+            console.log(stats);
+        });
+    });
+    reqest.end();
+}
 
 
 module.exports = router;
